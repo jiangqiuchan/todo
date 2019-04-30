f85584163f599714bcf1e36a44c3a1083259d3fe
江秋婵 <jiangqiuchan@miguantech.com>
2019-04-30 16:43:51 +0800

    配置文件

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/4/30
+ * Time: 16:40
+ */
+
+ //变量配置
+ define('DB_NAME','todo');
+ define('DB_USER','root');
+ define('DB_PASSWORD','root');
+ define('DB_HOST','localhost');
+ 
+ 
